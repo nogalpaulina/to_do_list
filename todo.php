2f2216d69e4d83b5a99b80aa74a028c80d9e5190
Paulina Nogal <nogalpaulina@gmail.com>
2015-05-03 14:41:31 -0400

    Add update and delete options

--- todo.php
+++ todo.php
@@ -4,7 +4,7 @@
 <title>To do list</title>
 </head>
 <body>
-
+<form method="post">
 <?php
 
 	//*  Connect to MySQL and Database
@@ -48,7 +48,7 @@
 		} else {
 			$completed= '';
 		}
-		if (empty($activity) || empty($deadline) || ($completed != '0' && $completed != '1'))
+		if (empty($activity) || empty($deadline))
 		{
 			print "<p style='color: red'>Must Fill Out All Fields Correctly</p>";
 		} else {
@@ -82,30 +82,57 @@
 		$outputDisplay  = "<h3>To do items:</h3>";
 
 		$outputDisplay .= '<table border=1 style="color: black;">';
-		$outputDisplay .= '<tr><th>Activity</th><th>Deadline</th><th>Completed</th></tr>';
+		$outputDisplay .= '<tr><th>Activity</th><th>Deadline</th><th>Completed</th><th>Delete?</th></tr>';
 
 		$numresults = mysqli_num_rows($result);
+		$numDisplayed = 0;
 
 		for ($i = 0; $i < $numresults; $i++)
 		{
-			if (!($i % 2) == 0)
-			{
-				 $outputDisplay .= "<tr style=\"background-color: lightgrey;\">";
-			} else {
-				 $outputDisplay .= "<tr style=\"background-color: white;\">";
-			}
 
 			$row = mysqli_fetch_array($result);
 
+			$id = $row['id'];
 			$activity = $row['activity'];
 			$deadline = $row['deadline'];
 			$completed = $row['completed'];
 
-			$outputDisplay .= "<td>".$activity."</td>";
-			$outputDisplay .= "<td>".$deadline."</td>";
-			$outputDisplay .= "<td>".$completed."</td>";
+			 if (isset($_POST['delete'.$id]))
+		        {
+		        	$delete = $_POST['delete'.$id];
+		        } else {
+		        	$delete = 'N';
+		        }
 
-			$outputDisplay .= "</tr>";
+			if ($delete == 'Y')
+			{
+				$statement = "DELETE FROM item WHERE id = $id";
+				mysqli_query($db, $statement);
+			} else {
+				if (isset($_POST['completed'.$id]))
+		        	{
+		        		$completed = 1;
+		        		$statement = "UPDATE item SET completed = $completed WHERE id = $id";
+					mysqli_query($db, $statement);
+		        	}
+
+				if ($numDisplayed % 2 == 0)
+				{
+					 $outputDisplay .= "<tr style=\"background-color: lightgrey;\">";
+				} else {
+					 $outputDisplay .= "<tr style=\"background-color: white;\">";
+				}
+				$strike = ($completed == 1) ? "<strike>" : "";
+				$outputDisplay .= "<td>".$strike.$activity."</td>";
+				$outputDisplay .= "<td>".$deadline."</td>";
+				$done_or_checkbox = ($completed == 1) ? "DONE" : "<input type='checkbox' name='completed".$id."' value='1'>";
+				$outputDisplay .= "<td>$done_or_checkbox</td>";
+				$outputDisplay .= "<td><input type='checkbox' name='delete".$id."' value='Y'></td>";
+
+				$outputDisplay .= "</tr>";
+
+				$numDisplayed++;
+			}
 
 		}
 
@@ -113,15 +140,17 @@
 
 	}
 
-	$outputDisplay .= "<br /><br /><b>Number of Rows in Results: $numresults </b><br /><br />";
+	$outputDisplay .= "<br /><br /><b>Number of Rows in Results: $numDisplayed </b><br /><br />";
 	print $outputDisplay;
 ?>
+	<input type="submit" value="Delete/Update items">
+	</form>
 
 	<h3>Add an item to your list:</h3>
 	<form method="post">
 	<input id="activity" name="activity" type="text" placeholder="Activity name...">
 	<input id="deadline" name="deadline" type="text" placeholder="2015-05-03">
-	<input id="completed" name="completed" type="text" placeholder="0 or 1">
+	Completed? <input type='checkbox' id="completed" name='completed' value='1'>
 	<input type="submit" name="addbutton" value="Add Item" />
 	</form>
 
